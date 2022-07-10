291934ed5cb52f164e7845cd56742d3cf7c596d5
Philipp Heuer <git@philippheuer.me>
2022-07-10 14:40:22 +0200

    feat: build buildah from source

--- common-minimal/buildah.sh
+++ common-minimal/buildah.sh
@@ -3,6 +3,7 @@
 container=$(buildah from registry.access.redhat.com/ubi9-minimal:latest)
 buildah config --label "-" $container
 buildah config --env HOME=/app $container
+buildah run $container useradd -u 1001 app --no-create-home --home-dir /app
 buildah run $container mkdir -p /app
 buildah run $container chgrp -R 0 /app
 buildah run $container chmod -R g=u /app
