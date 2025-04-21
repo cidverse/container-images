fc76efe675edeebc00fcd480b97b7549f9c57c27
Philipp Heuer <git@philippheuer.de>
2025-04-21 22:09:27 +0200

    feat: use gocover-cobertura with patch for file io

--- container-app/gocover-cobertura/default.nix
+++ container-app/gocover-cobertura/default.nix
@@ -2,12 +2,14 @@
   self,
   pkgs,
   pkgs-unstable,
+  inputs,
   ...
 }:
 
 let
   containerSupport = import (self + "/lib/container-support.nix") { inherit pkgs; };
-  rootPackage = pkgs-unstable.gocover-cobertura;
+  #rootPackage = pkgs-unstable.gocover-cobertura;
+  rootPackage = inputs.nixpkgs-philippheuer.packages.${pkgs.system}.gocover-cobertura;
 in
 {
   image-amd64 = containerSupport.buildImage {
